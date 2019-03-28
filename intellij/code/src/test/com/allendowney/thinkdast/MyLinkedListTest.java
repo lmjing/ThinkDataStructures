e324a817b939b5fa26c7a36d081a45d28c2ce25d
lmjing <lmijng1234@gmail.com>
2019-03-28 09:50:36 +0900

    실습을 위한 파일 생성

---
+++ intellij/code/src/test/com/allendowney/thinkdast/MyLinkedListTest.java
@@ -0,0 +1,22 @@
+package com.allendowney.thinkdast;
+
+import org.junit.Before;
+
+import java.util.ArrayList;
+
+public class MyLinkedListTest extends MyArrayListTest {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+
+        mylist = new MyLinkedList<Integer>();
+        mylist.addAll(list);
+    }
+}
