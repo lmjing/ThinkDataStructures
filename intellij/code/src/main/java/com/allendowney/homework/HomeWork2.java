293ff6a5ad58901ede2ef3ccd0a8b3cfd76c6982
lmjing <lmijng1234@gmail.com>
2019-04-01 21:53:44 +0900

    풀었으나 런타임 에러 뜸

---
+++ intellij/code/src/main/java/com/allendowney/homework/HomeWork2.java
@@ -0,0 +1,68 @@
+package com.allendowney.homework;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class HomeWork2 {
+
+    static int max = 0; // 자리 수 만큼 돌기 위함
+
+    public static void function(int div, Queue<Integer>[] queues) {
+        // 나눌 값이 max 보다 크면 그만
+        if (max < div) {
+            // 최종 값 출력
+            StringBuffer str = new StringBuffer();
+            for (Queue<Integer> q : queues) {
+                while (!q.isEmpty()) { // queue안에 있는거 다 꺼냄
+                    str.append(q.poll() + "\n");
+                }
+            }
+            System.out.println(str);
+            return;
+        }
+
+        // 무식하게 일단 10번 리셋
+        Queue<Integer>[] newQueues = new Queue[10];
+        for (int i=0; i<10; i++) {
+            newQueues[i] = new LinkedList<>();
+        }
+
+        // 0 ~ 9에 담겨있던 애들 꺼내서 다시 담기
+        for (Queue<Integer> q : queues) {
+            while (!q.isEmpty()) { // queue안에 있는거 다 꺼냄
+                int input = q.poll(); // 입력 값 꺼내서
+                int s = (input / div) % 10; // 그 다음 queue에 들어갈 위치
+                newQueues[s].add(input); // 그 다음 큐에 넣는다.
+            }
+        }
+        function(div * 10, newQueues);
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 몇 개인지 입력 받아서 그 크기만큼 배열 생성
+        int N = sc.nextInt();
+
+        // 무식하게 일단 10번 리셋
+        Queue<Integer>[] newQueues = new Queue[10];
+        for (int i=0; i<10; i++) {
+            newQueues[i] = new LinkedList<>();
+        }
+
+        // N 만큼 돌면서 입력 받기
+        for (int i=0; i<N; i++) {
+            // max 찾기
+            int input = sc.nextInt();
+            max = max < input ? input : max;
+
+            // 일단 1의 자리 기준으로 구함
+            int s = input % 10;
+            newQueues[s].add(input);
+        }
+
+        function(10, newQueues);
+    }
+}
