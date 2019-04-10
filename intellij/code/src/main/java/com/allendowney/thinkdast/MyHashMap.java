ce9316e2bf4e25902ae9a8439fb5bf0c845d5c68
lmjing <lmjing1234@gmail.com>
2019-04-10 15:01:15 +0900

    책보고 주석 추가

--- intellij/code/src/main/java/com/allendowney/thinkdast/MyHashMap.java
+++ intellij/code/src/main/java/com/allendowney/thinkdast/MyHashMap.java
@@ -18,6 +18,7 @@ import java.util.Map;
 public class MyHashMap<K, V> extends MyBetterMap<K, V> implements Map<K, V> {
 
 	// average number of entries per map before we rehash
+	// 로트 팩터 : 하위 맵당 최애 엔트리 개수 결정
 	protected static final double FACTOR = 1.0;
 
 	@Override
@@ -26,8 +27,10 @@ public class MyHashMap<K, V> extends MyBetterMap<K, V> implements Map<K, V> {
 
 		//System.out.println("Put " + key + " in " + map + " size now " + map.size());
 
+		// size() : 맵에 저장된 엔트리의 총 개수
 		// check if the number of elements per map exceeds the threshold
-		if (size() > maps.size() * FACTOR) {
+		// n > k * factor = n/k(하위 맵당 최대 엔트리 개수) > factor
+		if (size() > maps.size() * FACTOR) { // 하위 맵당 엔트리 개수 임계치 초과
 			rehash();
 		}
 		return oldValue;
@@ -35,12 +38,21 @@ public class MyHashMap<K, V> extends MyBetterMap<K, V> implements Map<K, V> {
 
 	/**
 	 * Doubles the number of maps and rehashes the existing entries.
+	 * rehash 메서드 호출 시마다 내장된 맵의 개수 k가 두 배가 되어야 함
 	 */
 	/**
 	 *
 	 */
 	protected void rehash() {
 		// TODO: FILL THIS IN!
+		// 테이블에서 엔트리 수집, 크기 산정, 엔트리 다시 넣기
+
+		makeMaps(maps.size()); // 하위 맵 개수 2배로 늘려줌
+		for (MyLinearMap map : maps) {
+			for (Object entry : map.getEntries()) {
+
+			}
+		}
 	}
 
 	/**
