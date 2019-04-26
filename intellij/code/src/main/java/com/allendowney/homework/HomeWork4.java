e940e16aff7f2bff43efa3320b4ba350ee6325c6
lmjing <lmjing1234@gmail.com>
2019-04-26 11:01:32 +0900

    구현 완료 - 백준 테스트 해볼 것

--- intellij/code/src/main/java/com/allendowney/homework/HomeWork4.java
+++ intellij/code/src/main/java/com/allendowney/homework/HomeWork4.java
@@ -1,9 +1,9 @@
 package com.allendowney.homework;
 
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+        import java.util.Scanner;
 
 public class HomeWork4 {
 
