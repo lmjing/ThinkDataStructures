78a3b17812bf107944891a0d39f77e9924e1dfb8
lmjing <lmjing1234@gmail.com>
2019-04-26 10:38:13 +0900

    문제해결

--- intellij/code/src/main/java/com/allendowney/homework/HomeWork4.java
+++ intellij/code/src/main/java/com/allendowney/homework/HomeWork4.java
@@ -22,52 +22,113 @@ public class HomeWork4 {
     static int[] tree = new int[10001];
     static StringBuilder result = new StringBuilder();
 
+    static class Tree {
+        Node root;
+
+        public void addNode(int v) {
+            // root일 때
+            if (root == null) {
+                root = new Node(v);
+                return;
+            }
+
+            // root부터 차례대로 비교하며 위치에 삽입
+            Node cur = root;
+            Node parent = null;
+            int d = -1;
+            // 찾는 위치가 비었을 때 멈춤
+            while (cur != null) {
+                parent = cur;
+                // 삽입될 값이 비교노드 값 보다 작거나 같다 -> 오른쪽으로 이동
+                if (cur.value <= v) {
+                    cur = cur.rightChild;
+                    d = 1;
+                }else { // 크다 -> 왼쪽으로 이동
+                    cur = cur.leftChild;
+                    d = 0;
+                }
+            }
+
+            if (d > 0)
+                parent.rightChild = new Node(v);
+            else
+                parent.leftChild = new Node(v);
+        }
+
+        class Node {
+            int value;
+            Node leftChild;
+            Node rightChild;
+
+            public Node(int v) {
+                this.value = v;
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         Scanner sc = new Scanner(System.in);
 
+        Tree tree = new Tree();
         /**
          * 백준 Test코드
          */
-        while(sc.hasNextLine()){
-            addTree(Integer.parseInt(sc.nextLine()));
-        }
+//        while(sc.hasNextLine()){
+//            tree.addNode(Integer.parseInt(sc.nextLine()));
+////            addTree(Integer.parseInt(sc.nextLine()));
+//        }
 
         /**
          * 개인 Test코드
          */
         // 들어오는 순서대로 처리한다.
-//        while (true) {
-//            String input = br.readLine();
-//            // TODO : 입력 받는걸 못하겠어서... 걍 엔터 한번 더 누르면 그만 입력 받게 함
-//            if (input == null || input.equals(""))
-//                break;
-//
-//            // 트리를 만들기 위해 데이터를 넣어준다.
+        while (true) {
+            String input = br.readLine();
+            // TODO : 입력 받는걸 못하겠어서... 걍 엔터 한번 더 누르면 그만 입력 받게 함
+            if (input == null || input.equals(""))
+                break;
+
+            // 트리를 만들기 위해 데이터를 넣어준다.
+            tree.addNode(Integer.parseInt(input));
 //            addTree(Integer.parseInt(input));
-//        }
+        }
 
         // 후위 순회로 결과 출력
-        makeResult();
+//        makeResult();
+        postOrder(tree.root);
         // 마지막 '\n' 삭제
         System.out.println(result.delete(result.length()-1, result.length()));
     }
 
-    private static void makeResult() {
-        postOrder(1);
-    }
+//    private static void makeResult() {
+//        postOrder(1);
+//    }
 
-    private static void postOrder(int index) {
+//    private static void postOrder(int index) {
+//        // 가르키는 노드가 없을 때 리턴
+//        if (tree[index] == 0)
+//            return;
+//
+//        // 왼쪽부터 호출 => 자신의 * 2
+//        postOrder(index * 2);
+//        // 오른쪽 호출 => 자신 * 2 + 1
+//        postOrder(index * 2 + 1);
+//        // 자신 저장
+//        result.append(tree[index] + "\n");
+//    }
+
+    private static void postOrder(Tree.Node node) {
         // 가르키는 노드가 없을 때 리턴
-        if (tree[index] == 0)
+        if (node == null)
             return;
 
         // 왼쪽부터 호출 => 자신의 * 2
-        postOrder(index * 2);
+        postOrder(node.leftChild);
         // 오른쪽 호출 => 자신 * 2 + 1
-        postOrder(index * 2 + 1);
+        postOrder(node.rightChild);
         // 자신 저장
-        result.append(tree[index] + "\n");
+        result.append(node.value + "\n");
     }
 
     private static void addTree(int value) {
